8920853d86a1f92200f6eefca5501a83fcfb4c99
Erfang Chen <erfangc@gmail.com>
2023-01-12 19:45:26 -0500

    initial commit

---
+++ example/main.tf
@@ -0,0 +1,30 @@
+data "aws_vpc" "default_vpc" {
+  default = true
+}
+
+data "aws_subnet_ids" "subnet_ids" {
+  vpc_id = data.aws_vpc.default_vpc.id
+}
+
+resource "aws_dynamodb_table" "requests" {
+  name         = "requests"
+  billing_mode = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "Id"
+    type = "S"
+  }
+
+  stream_enabled   = true
+  stream_view_type = "NEW_AND_OLD_IMAGES"
+  hash_key         = "Id"
+}
+
+module "stream-reader" {
+  source                          = "../"
+  subnet_ids                      = data.aws_subnet_ids.subnet_ids.ids
+  vpc_id                          = data.aws_vpc.default_vpc.id
+  dynamodb_table_name             = aws_dynamodb_table.requests.name
+  dynamodb_user_id_attribute_name = "updatedBy"
+  depends_on                      = [aws_dynamodb_table.requests]
+}
--- main.tf
+++ main.tf
@@ -1,30 +1,45 @@
-data "aws_vpc" "default_vpc" {
-  default = true
+resource "aws_cloudwatch_log_group" "function_log_group" {
+  name              = "/aws/lambda/${aws_lambda_function.stream_reader.function_name}"
+  retention_in_days = 7
+  lifecycle {
+    prevent_destroy = false
+  }
 }
 
-data "aws_subnet_ids" "subnet_ids" {
-  vpc_id = data.aws_vpc.default_vpc.id
-}
+resource "aws_lambda_function" "stream_reader" {
 
-resource "aws_dynamodb_table" "requests" {
-  name         = "requests"
-  billing_mode = "PAY_PER_REQUEST"
+  function_name    = "${var.dynamodb_table_name}-dynamodb-history-recorder"
+  role             = aws_iam_role.stream_reader_lambda_role.arn
+  filename         = "lambda_code.zip"
+  source_code_hash = data.archive_file.python_lambda_package.output_base64sha256
+  handler          = "lambda_function.lambda_handler"
 
-  attribute {
-    name = "Id"
-    type = "S"
+  environment {
+    variables = {
+      "DYNAMODB_PARTITION_KEY_ATTRIBUTE_NAME" = data.aws_dynamodb_table.dynamodb_table.hash_key
+      "DYNAMODB_SORT_KEY_ATTRIBUTE_NAME"      = data.aws_dynamodb_table.dynamodb_table.range_key
+      "DYNAMODB_HISTORY_TABLE_NAME"           = aws_dynamodb_table.history_table.name
+      "DYNAMODB_USER_ID_ATTRIBUTE_NAME"       = var.dynamodb_user_id_attribute_name
+    }
   }
 
-  stream_enabled   = true
-  stream_view_type = "NEW_AND_OLD_IMAGES"
-  hash_key         = "Id"
+  vpc_config {
+    security_group_ids = [aws_security_group.sg.id]
+    subnet_ids         = var.subnet_ids
+  }
+
+  runtime = "python3.9"
 }
 
-module "stream-reader" {
-  source                                = "./dynamodb-history-recorder"
-  subnet_ids                            = data.aws_subnet_ids.subnet_ids.ids
-  vpc_id                                = data.aws_vpc.default_vpc.id
-  dynamodb_table_name                   = aws_dynamodb_table.requests.name
-  dynamodb_user_id_attribute_name       = "updatedBy"
-  depends_on                            = [aws_dynamodb_table.requests]
+resource "aws_lambda_event_source_mapping" "stream_reader_lambda_event_source_mapping" {
+  function_name     = aws_lambda_function.stream_reader.arn
+  starting_position = "LATEST"
+  event_source_arn  = data.aws_dynamodb_table.dynamodb_table.stream_arn
+}
+
+
+data "archive_file" "python_lambda_package" {
+  output_path = "lambda_code.zip"
+  type        = "zip"
+  source_file = "${path.module}/code/lambda_function.py"
 }
